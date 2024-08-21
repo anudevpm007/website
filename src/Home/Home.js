5955e0d397df048ccbf98084cebd3d0a1964052f
anudevpm007 <anudevpm007@gmail.com>
2024-08-21 22:32:04 +0530

    first

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, CardImg } from "react-bootstrap";
 // import { useEffect } from "react";
 import "./Home.css";
 import "./Home_body.css";
@@ -11,22 +11,8 @@ import services_data from "../Data/Services_data";
 import Services from "./Home_body/services/services";
 
 export default function Home() {
-  // render() {
 
-  // var url = window.location.pathname;
-  // url = url+"Hello3"
-  // useEffect(() => {
-  //   url = url+"Hello2"
-  //   if (url == "/website/from=blog/") {
-  //     url = url+"Hello"
-  //     let availableWidthPx = document.getElementById("service_list");
-  //     if (availableWidthPx) {
-  //       console.log(availableWidthPx.offsetWidth);
-  //     }
-  //   }
-  // }, []);
-
-  function close(){
+  function close() {
     document.getElementById("sidebar-active").click()
 
   }
@@ -84,61 +70,50 @@ export default function Home() {
         </div>
         <div id="Rest">
           <div id="D_P_up">
-            <Button as="a" href="#home" id="B_P_up"><img src={require("../images/UP.png")}/></Button>
+            <Button as="a" href="#home" id="B_P_up"><img src={require("../images/UP.png")} /></Button>
           </div >
           <div id="B_P_down">
-          <Button as="a" id="B_P_down" href="#down"><img src={require("../images/DOWN.png")}/></Button>
-          </div> 
-          
+            <Button as="a" id="B_P_down" href="#down"><img src={require("../images/DOWN.png")} /></Button>
+          </div>
+
+          {/* Header Section */}
+
           <div id="Header_L">
-            <div className="container">
-              <div className="row" id="r">
-                <div className="col-7" id="heading-L">
-                  <span>
-                    <h1>Integrity and Innovation in Cyber Security </h1>
-                  </span>
-                  <span>
-                    <p>
-                      At ingokSek , we prioritize integrity and innovation in
-                      cyber security. We're a research-driven organization,
-                      developing new solutions and leveraging advanced
-                      technologies to protect your digital assets. Our ethical
-                      approach ensures trust and reliability, offering
-                      personalized solutions to meet your specific needs.
-                      Discover how ingokSek can enhance your cyber security
-                      strategy with our advanced expertise and commitment to
-                      excellence.
-                    </p>
-                  </span>
-                  {/* <span><h6>Research Focused, In-Depth & Resilient Security Assessments For Applications, Cloud & Networks</h6></span> */}
+            <div className="row" id="H_div_mian">
+              <div className="col" id="H_Col1">
+                <h3>Integrity and Innovation in Cyber Security</h3>
+                At ingokSek , we prioritize integrity and innovation in
+                cyber security. We're a research-driven organization,
+                developing new solutions and leveraging advanced
+                technologies to protect your digital assets. Our ethical
+                approach ensures trust and reliability, offering
+                personalized solutions to meet your specific needs.
+                Discover how ingokSek can enhance your cyber security
+                strategy with our advanced expertise and commitment to
+                excellence.
+              </div>
+              <div className="col" id="H_Col3">
+                <div>
+                  <h3>Integrity and Innovation in Cyber Security</h3>
+                  At ingokSek , we prioritize integrity and innovation in
+                  cyber security. We're a research-driven organization,
+                  developing new solutions and leveraging advanced
+                  technologies to protect your digital assets. Our ethical
+                  approach ensures trust and reliability, offering
+                  personalized solutions to meet your specific needs.
+                  Discover how ingokSek can enhance your cyber security
+                  strategy with our advanced expertise and commitment to
+                  excellence.
                 </div>
-                <div className="col-10" id="heading-L2">
-                  <span>
-                    <h1>Integrity and Innovation in Cyber Security</h1>
-                  </span>
-                  <span>
-                    <p>
-                      At ingokSek , we prioritize integrity and innovation in
-                      cyber security. We're a research-driven organization,
-                      developing new solutions and leveraging advanced
-                      technolog ies to protect your digital assets. Our ethical
-                      approach ensures trust and reliability, offering
-                      personalized solutions to meet your specific needs.
-                      Discover how ingokSek can enhance your cyber security
-                      strategy with our advanced expertise and commitment to
-                      excellence.
-                    </p>
-                  </span>
-                  {/* <span><h6>Research Focused, In-Depth & Resilient Security Assessments For Applications, Cloud & Networks</h6></span> */}
-                </div>
-                <div className="col-5" id="img-div">
-                  <img src={require("../images/AB3.png")} alt="N?A" id="IMG" />
-                </div>
+              </div>
+              <div className="col" id="H_Col2">
+                <CardImg src={require("../images/AB3.png")} />
               </div>
             </div>
           </div>
-          
+
           <div className="Home_body">
+
             <div className="container-fluid">
               <div className="row">
                 <div id="service_home">
@@ -153,6 +128,7 @@ export default function Home() {
                         backgroundColor: "transparent",
                       }}
                     >
+
                       Services
                     </div>
                     <div className="row" id="list">
@@ -171,7 +147,7 @@ export default function Home() {
             </div>
           </div>
           <div id="down">
-          <Home_footer  />
+            <Home_footer />
           </div>
         </div>
       </div>
