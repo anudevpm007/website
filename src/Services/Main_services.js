31c57ceb183337365a3acfbc03088ad77de8bbf7
anudevpm007 <anudevpm007@gmail.com>
2024-08-25 14:06:28 +0530

    first

--- src/Services/Main_services.js
+++ src/Services/Main_services.js
@@ -4,6 +4,8 @@ import Services_Main_data from "../Data/Service_Main_data";
 import { CardImg, CardText, CardTitle } from "react-bootstrap";
 import Menu from "../Menu/Menu";
 import Home_footer from "../Home/Home_footer/Home_footer";
+import { Link } from "react-router-dom";
+
 
 export default class Main_services extends Component {
   render() {
@@ -121,7 +123,57 @@ export default class Main_services extends Component {
                 </div>
               </div>
             </div>
-            <div className="container" id="small_Service">
+            <div id="ser_table">
+              <table id="ser_table_1">
+                <tr>
+                  <td className="ser_table11_1"><div className="ST_col1">Comprehensive Security Analysis</div></td>
+                  <td><div id="hor_space"></div></td>
+                  <td className="ser_table11_1"><div className="ST_col2">Conducting thorough evaluations of IoT devices involves assessing their software, hardware, and network interactions. This process uncovers vulnerabilities that could be exploited, ensuring devices are secure against various types of attacks.</div></td>
+                </tr>
+
+                <tr>
+                  <div></div>
+                </tr>
+                <tr>
+                  <td className="ser_table11_1"><div className="ST_col1">Hardware and Firmware Exploitation</div></td>
+                  <td><div id="hor_space"></div></td>
+                  <td className="ser_table11_1"><div className="ST_col2">This involves discovering and exploiting security weaknesses in both the physical hardware and its firmware. Attackers might manipulate hardware interfaces or firmware to gain unauthorized access or disrupt functionality.</div></td>
+                </tr>
+                <tr>
+                  <div></div>
+                </tr>
+                <tr>
+                  <td className="ser_table11_1"><div className="ST_col1">Low-Level Vulnerability Assessment</div></td>
+                  <td><div id="hor_space"></div></td>
+                  <td className="ser_table11_1"><div className="ST_col2">Performing in-depth analysis at the firmware level involves scrutinizing the code and architecture for flaws. This is crucial for identifying vulnerabilities that can be exploited at a fundamental level, often leading to severe security breaches.</div></td>
+                </tr>
+                <tr>
+                  <div></div>
+                </tr>
+                <tr>
+                  <td className="ser_table11_1"><div className="ST_col1">Black-Box and White-Box Testing</div></td>
+                  <td><div id="hor_space"></div></td>
+                  <td className="ser_table11_1"><div className="ST_col2">Black-box testing examines a system's functionality without prior knowledge, simulating external attacks. White-box testing, in contrast, involves a thorough examination of internal structures and code, offering a more detailed evaluation of security risks</div></td>
+                </tr>
+                <tr>
+                  <div></div>
+                </tr>
+                <tr>
+                  <td className="ser_table11_1"><div className="ST_col1">IoT Ecosystem Security</div></td>
+                  <td><div id="hor_space"></div></td>
+                  <td className="ser_table11_1"><div className="ST_col2">Analyzing the broader IoT ecosystem includes evaluating related components like mobile apps, web services, and cloud infrastructure. This approach ensures that all interconnected elements are secure and not vulnerable to breaches that could impact the IoT system.</div></td>
+                </tr>
+                <tr>
+                  <div></div>
+                </tr>
+                <tr>
+                  <td className="ser_table11_1"><div className="ST_col1">Detailed Reporting and Recommendations</div></td>
+                  <td><div id="hor_space"></div></td>
+                  <td className="ser_table11_1"><div className="ST_col2">Providing detailed reports involves documenting discovered vulnerabilities and their implications. Recommendations for remediation are also included, offering actionable steps to address and mitigate the identified security issues effectively.</div></td>
+                </tr>
+              </table>
+            </div >
+            {/* <div className="container" id="small_Service">
               <div className="row" id="small_Service_1">
                 {this.state.S_Main_heading.map((data, i) => {
                   return (
@@ -148,8 +200,27 @@ export default class Main_services extends Component {
                 })}
 
               </div>
+            </div> */}
+            <div id="Under_table_pera">
+              <p>Your IoT devices are critical to your operations, and ensuring their security is paramount. Our expert team specializes in comprehensive security analysis to identify and mitigate vulnerabilities, ensuring your systems are protected against potential threats. Don't leave your security to chance—book your free consultation today and take the first step towards a safer, more secure IoT environment.</p>
+            </div>
+
+            <div id="book_now">
+              <div className="row" id="book_box_1">
+                <div className="col-6" id="book_box_1_1">
+                  <h3>Book Your Free Concultency</h3>
+                </div>
+                <div className="col-6" id="book_box_1_2">
+                  <button >
+                    <Link to={"/website/Book/"} id="Book_Link">
+                      Book Now
+                    </Link>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
+
           <div>
             <Home_footer />
           </div>
