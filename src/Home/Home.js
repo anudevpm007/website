0951d617ba98f76c54303e214cea46f05336226a
anudevpm007 <anudevpm007@gmail.com>
2024-09-14 23:31:14 +0530

    first

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Menu from '../Menu/Menu'
 import "./Home.css"
 import { Link } from 'react-router-dom'
@@ -9,23 +9,64 @@ const sub = (event) => {
   // console.log(email);
   event.preventDefault();
   let email = document.getElementById('sub_email').value;
-  let data = {email:email};
+  let data = { email: email };
   console.log(data.length)
   if (data.length !== 0) {
-    axios.post("http://localhost:8000/", data).then(res => console.log("Status Code :"+res.status)).catch(err => console.log("error"));
-    
+    axios.post("http://localhost:8000/", data).then(res => console.log("Status Code :" + res.status)).catch(err => console.log("error"));
+
   }
 }
 
-export default class Home extends Component {
-  render() {
-    return (
+var i = 0;
+
+
+function More() {
+  return (
+    <div id='service_box_row_3' className='row'>
+      <div id='service_box_5' className='col-md'>
+        <Link>
+          <div>
+            <img src={require("./image/Backup.png")} />
+            <p>Data Recovery</p>
+          </div>
+        </Link>
+      </div>
+      <div id='service_box_6' className='col-md'>
+        <Link>
+          <div>
+            <img src={require("./image/server.png")} />
+            <p>DataBase</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default function Home() {
+  const [show, setShow] = useState("View More");
+  const [serstate, setSerstate] = useState(false);
+
+  const serviceClick = () => {
+    i = i + 1
+    if (i % 2 == 1) {
+      setShow("Hide")
+      setSerstate(true)
+    } else if (i % 2 == 0) {
+      setShow("View More")
+      setSerstate(false)
+    }
+
+  }
+  return (
+    <div>
+      <div id='Home_menu'>
+        <Menu />
+      </div>
       <div id='Home'>
 
         <div id='Home_container'>
-          <div id='Home_menu'>
-            <Menu />
-          </div>
+
           <div id='Home_heading'>
             <div>
               <div id='Home_top_heading'>
@@ -79,6 +120,12 @@ export default class Home extends Component {
                       </Link>
                     </div>
                   </div>
+                  {
+                    serstate && <More />
+                  }
+                  <div id='show_service'>
+                    <button onClick={serviceClick}>{show}</button>
+                  </div>
                 </div>
                 <div id='service_Section_2' className='col-5'>
                   <div>
@@ -88,9 +135,18 @@ export default class Home extends Component {
                 </div>
               </div>
             </div>
+            <div id='why_box' className='row'>
+              <div id='why_box_1' className='col-10'>
+                <h2 >Why INKOGSEK</h2>
+                <p>We're a research-driven organization, developing new solutions and leveraging advanced technologies to protect your digital assets. Our ethical approach ensures trust and reliability, offering personalized solutions to meet your specific needs. Discover how ingokSek can enhance your cyber security strategy with our advanced expertise and commitment to excellence.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    )
-  }
+      <div id='H_footer'>
+        <Home_footer />
+      </div>
+    </div>
+  )
 }
