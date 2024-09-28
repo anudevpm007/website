f2027cc5e980d66bb32827032133a5c34eb13a43
anudevpm007 <anudevpm007@gmail.com>
2024-09-28 20:39:24 +0530

    first

--- src/Carrier/Data/Data_Carrier.js
+++ src/Carrier/Data/Data_Carrier.js
@@ -1,8 +1,8 @@
 const Carrier_data = [
-    {id: 1, heading: "Application Security - Intern", pera: "Carrier job peragraph 1", loc: "Bangalure",exp:"   0-1 year"},
-    {id: 2, heading: "Carrier job heading hello 2", pera: "Carrier job peragraph 2", loc: "Kozhikode",exp:"   0-1 year"},
-    {id: 3, heading: "Carrier job heading how  3", pera: "Carrier job peragraph 3", loc: "Mysuru",exp:"   0-1 year"},
-    {id: 4, heading: "Carrier job heading  where 4", pera: "Carrier job peragraph 4", loc: "Kozhikode",exp:"   0-1 year"},
+    {id: 1, heading: "Application Security - Intern", pera: "you need to work with various security software, including network security monitoring tools, packet sniffers, vulnerability scanners, firewalls, and antivirus software", loc: "Bangalure, Karnataka",exp:"   0-1 year"},
+    {id: 2, heading: "network security testing", pera: "the process of searching for potential security issues in a network. These can contain software vulnerabilities, insecure protocols, misconfigurations, and other errors that place the organization at risk of exploitation.", loc: "Kozhikode, Kerala",exp:"   0-1 year"},
+    {id: 3, heading: "Penetration testing ", pera: "security exercise where a cyber-security expert attempts to find and exploit vulnerabilities in a computer system.", loc: "Mysuru, Karnataka",exp:"   0-1 year"},
+    {id: 4, heading: "mobile application security ", pera: "Mobile app security is the practice of safeguarding high-value mobile applications and your digital identity from fraudulent attack in all its forms.", loc: "Ernakulam, Kerala",exp:"   0-1 year"},
     {id: 5, heading: "Carrier job heading where 5", pera: "Carrier job peragraph 5", loc: "Bangalure",exp:"   0-1 year"}
 ]
 
