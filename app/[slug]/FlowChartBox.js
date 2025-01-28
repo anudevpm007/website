ce2cabc6cdac37b61f314e9c6044ac26e929d9de
anudevpm007 <anudevpm007@gmail.com>
2025-01-28 23:21:02 +0530

    Delete app directory

--- app/[slug]/FlowChartBox.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function FlowChartBox() {
-  return (
-    <div>FlowChartBox</div>
-  )
-}
--- app/[slug]/QAdb.js
+++
@@ -1,9 +0,0 @@
-const QAdb = [
-  {
-    id: 1,
-    Q: "What is web application security testing ?",
-    A: "Web application security testing involves identifying vulnerabilities in your web applications to prevent potential attacks. This includes testing for weaknesses in your application's code, business logic, third-party components, and known security flaws (CVEs).",
-  },
-];
-
-export default QAdb;
