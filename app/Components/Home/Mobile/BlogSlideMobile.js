842876d8c09c00a5ac848b2ca85f3fed42c45087
anudevpm007 <anudevpm007@gmail.com>
2025-02-21 10:19:55 +0530

    commmit

--- app/Components/Home/Mobile/BlogSlideMobile.js
+++ app/Components/Home/Mobile/BlogSlideMobile.js
@@ -3,41 +3,51 @@ import Image from "next/image";
 import BlogDataCard from "@/app/db/DBCardData";
 import Link from "next/link";
 
-
-const MobCard = ({heading, pera, image , index }) => {
-    return (
-        <div className="w-[400px] md:w-[600px] lg:w-[800px]  border-[#0000006b] border-[2px] rounded-2xl md:rounded-3xl lg:rounded-[40px] ">
-            <Image src={image} alt="IMage 1 Blog" className="w-full" />
-            <div className="p-5 md:p-10">
-                <h1 className="text-[5vw] md:text-[5.4vw] lg:text-[4.8vw] font-heading ">{heading}</h1>
-                <p className="mb-12 font-body text-[3vw] md:text-[2.8vw] lg:text-[2.3vw] text-justify">{pera}</p>
-                <Link href={{
-            pathname:"/Blog/"+heading,
-            query:{
-              Index:index
-            }
-          }} className=" lg:rounded-lg md:rounded-md rounded-sm bg-[#185BD8] px-8 py-2 text-white">Read More</Link>
-            </div>
-           
-        </div>
-    )
-}
+const MobCard = ({ heading, pera, image, index }) => {
+  return (
+    <div className="relative w-[400px] md:w-[600px] lg:w-[800px]  border-[#0000006b] border-[2px] rounded-2xl md:rounded-3xl lg:rounded-[40px] ">
+      <Image src={image} alt="IMage 1 Blog" className="rounded-t-xl w-full" />
+      <div className="p-5 md:p-10">
+        <h1 className="text-[5vw] md:text-[5.4vw] lg:text-[4.8vw] font-heading ">
+          {heading}
+        </h1>
+        <p className="mb-20 font-body text-[3vw] md:text-[2.8vw] lg:text-[2.3vw] text-justify">
+          {pera}
+        </p>
+      </div>
+      <Link
+        href={{
+          pathname: "/Blog/" + heading,
+          query: {
+            Index: index,
+          },
+        }}
+        className="absolute bottom-5 left-8 lg:rounded-lg md:rounded-md rounded-sm bg-[#185BD8] px-8 py-2 text-white"
+      >
+        Read More
+      </Link>
+    </div>
+  );
+};
 
 export default function BlogSlideMobile() {
-    return (
-        <div>
-            <div className="mt-8 mb-10 flex justify-end">
-                <div className="w-[95%] scrollbar-hide grid gap-5 grid-flow-col overflow-scroll">
-                    {
-                        BlogDataCard.map((data,index)=>{
-                            return(
-                                <MobCard heading={data.Heading} image={data.img} index={index} pera={data.pera} key={index}/>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="mt-8 mb-10 flex justify-end">
+        <div className="w-[95%] scrollbar-hide grid gap-5 grid-flow-col overflow-scroll">
+          {BlogDataCard.map((data, index) => {
+            return (
+              <MobCard
+                heading={data.Heading}
+                image={data.img}
+                index={index}
+                pera={data.pera}
+                key={index}
+              />
+            );
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
--- app/Components/Home/Mobile/MobServ.js
+++ app/Components/Home/Mobile/MobServ.js
@@ -24,7 +24,7 @@ export default function MobServ() {
                             <div key={Index} className="w-[100vw] p-3 md:px-8 md:py-5 rounded-md bg-white">
                                 <div>
                                     <h1 className="text-[6vw] md:text-[4vw] lg:text-[3.5vw] font-heading text-[#0858ED]">{Data.Main}</h1>
-                                    <p className="text-[3vw] md:text-[2vw] lg:text-[1.8vw] mt-4">Our experts deliver advanced application security testing, identifying critical vulnerabilities like CVEs and business logic flaws to protect your application and protect your brand's reputation.</p>
+                                    <p className="text-[3vw] md:text-[2vw] lg:text-[1.8vw] mt-4">{Data.pera}</p>
 
                                     <ul className="mt-4 ">
                                         {
